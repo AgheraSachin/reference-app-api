66c1a5d94ddf036ba6d51ce2039db79fb6ff2527
Sachin Aghera <sachinaghera4@gmail.com>
2021-05-12 17:21:42 +0530

    url set for audio video

--- app/Models/VerifiedRatingRequest.php
+++ app/Models/VerifiedRatingRequest.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class VerifiedRatingRequest extends Model
 {
@@ -13,5 +14,19 @@ class VerifiedRatingRequest extends Model
 
     public function user(){
         return $this->hasOne(User::class,'id','to_user_id');
+    }
+
+    public function getVideoAttribute($value)
+    {
+        if ($value != null) {
+            return Storage::url('Video/' . $value);
+        }
+    }
+
+    public function getAudioAttribute($value)
+    {
+        if ($value != null) {
+            return Storage::url('Audio/' . $value);
+        }
     }
 }
